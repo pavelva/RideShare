8904942b8188f2622351283efad75b63b125b204
Alex21zel <Alex21zel@gmail.com>
2015-03-03 17:18:26 +0200

    alex

---
+++ Scripts/publishRide.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Alex on 3/2/2015.
+ */
+
+//$(document).ready(function() {
+//
+//    $(".publishPostRide").append(addPublishDetails());
+//
+//});
+
+//function changeToPublishRide(){
+//            $(".myRides").slideUp(700);
+//    $(".publishPostRide").append(addPublishDetails());
+//    $(".publishPostRide").show();
+//}
+
+function addPublishDetails(){
+    return "" +
+        '<div class="publishPostRide container">'+
+            '<div class="publishPostRideHeader">'+
+                'Publish New Ride'+
+        '</div>'+
+        '<div class="publishPostContent">'+
+            '<p class="label">'+
+                '<span class="text">From :</span>'+
+                '<input type="text" class="inputText" style="margin-top: 50px"/>'+
+            '</p>'+
+            '<p class="label">'+
+                '<span class="text">To :</span>'+
+                '<input type="text" class="inputText" style="margin-bottom: 50px"/>'+
+                '</p>'+
+            '<p class="label">'+
+                '<span class="text">Date :</span>'+
+                '<input type="date" class="inputText dateInput" style="margin-top: 50px"/>'+
+            '</p>'+
+                '<p class="label">'+
+                '<span class="text">From Time :</span>'+
+            '<input type="time" class="inputText timeInput" />'+
+            '</p>'+
+                '<p class="label">'+
+                '<span class="text">To Time:</span>'+
+            '<input type="time" class="inputText timeInput" />'+
+            '</p>'+
+                '<p class="label">'+
+                '<input type="button" class="publishPostButton btn" value="Publish"/>'+
+            '</p>'+
+        '</div>'+
+    '</div>'
+}
