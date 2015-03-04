20aa1d446371c149a916c99f7442cd4c23cb294b
Alex21zel <Alex21zel@gmail.com>
2015-03-04 22:08:34 +0200

    alex - post ride

--- Scripts/publishRide.js
+++ Scripts/publishRide.js
@@ -24,58 +24,153 @@ function addPublishDetails(){
         '<div class="publishPostRideHeader">'+
                 'Publish New Ride'+
         '</div>'+
-        '<div id="publishPostContent">'+
+        '<div id="publishPostContent" onload="onLoad()">'+
             '<p class="label">'+
                 '<span class="text">From :</span>'+
-                '<input type="text" class="inputText" />'+
+                '<input type="text" name="input" class="inputText" />'+
             '</p>'+
             '<p class="label">'+
                 '<span class="text">To :</span>'+
-                '<input type="text" class="inputText" />'+
+                '<input type="text" name="input" class="inputText" />'+
             '</p>'+
             '<p class="label">'+
                 '<span class="text">Stopover :</span>'+
-                '<input type="text" id="inputStopOver" class="inputText"/>'+
+                '<input type="text" id="inputStopOver" name="input" class="inputText" onkeypress="handleEnterKey(event)"/>'+
                 '<input type="button" id="submitButton" class="btn" value="+" onclick="addStopOver()"/>'+
             '</p>'+
-            '<p id="outPutLabel" class="label">'+
-                '<output id="outPutText" /></output>'+
+            '<p class="label">'+
+                '<span class=outPutText>'+
+                    '<span id=firsOut></span>'+
+                    '<input type="button" id="firstButton" class="clearBtn btn a" value="X"  onclick="removeElement(0)"/>'+
+                    '<span id=secondOut></span>'+
+                    '<input type="button" id="secondButton" class="clearBtn btn a" value="X" onclick="removeElement(1)"/>'+
+                    '<span id=thirdOut></span>'+
+                    '<input type="button" id="thirdButton" class="clearBtn btn a" value="X" onclick="removeElement(2)"/>'+
+                    '<span id=fourthOut></span>'+
+                    '<input type="button" id="fourthButton" class="clearBtn btn a" value="X" onclick="removeElement(3)"/>'+
+                    '<span id=fifthOut></span>'+
+                    '<input type="button" id="fifthButton" class="clearBtn btn a" value="X" onclick="removeElement(4)"/>'+
+                '</span>'+
             '</p>' +
             '<p class="label">'+
                 '<span class="text">Date :</span>'+
-                '<input type="date" class="inputText dateInput" />'+
+                '<input type="date" id="dateInput" name="Date" class="inputText" />'+
             '</p>'+
             '<p class="label">'+
                 '<span class="text">From Time :</span>'+
-                '<input type="time" class="inputText timeInput" />'+
+                '<input type="time" id="fromTimeInput" class="inputText timeInput" />'+
             '</p>'+
             '<p class="label">'+
                 '<span class="text">To Time:</span>'+
-                '<input type="time" class="inputText timeInput" />'+
+                '<input type="time" id="toTimeInput" class="inputText timeInput" />'+
             '</p>'+
             '<p class="label">'+
                 '<input type="button" class="publishPostButton btn" value="Publish"/>'+
-                '<input type="button" class="clearButton btn" value="Clear"/>'+
+                '<input type="button" class="clearButton btn" value="Clear" onclick="clearAllFields()"/>'+
             '</p>'+
         '</div>'
 }
-var c =0;
+
+var c = 0;
+var elemArr = ["#firsOut","#secondOut","#thirdOut","#fourthOut","#fifthOut"];
+var btnArr = ["#firstButton","#secondButton","#thirdButton","#fourthButton","#fifthButton"];
 function addStopOver(){
-    if(c<5){
-        var element = $("#inputStopOver");
-        out = element.val();
-        element.val("");
-        $("#outPutLabel").append('<p><input type="button" class="btn" style="height:22px; width: 40px; margin-left: 270px; margin-top: 5px" value="Clear" onclick="removeItem(c)"></p>');
-        $("#outPutText").append('<p id="'+c+'">'+out+'</p>');
-        //'<input type="button" style="height:50px" value="Clear" '+
-        //'onclick="alert( $("main").hide();)"/></p>');
-        c++;
+    var inElement = $("#inputStopOver");
+    var out = inElement.val();
+    if(out != "") {
+        if (c < 5) {
+            inElement.val("");
+            $(elemArr[c]).text(out).show();
+            $(btnArr[c]).show();
+            c++;
+        }
+        else {
+            alert("can't add more than five stopover")
+        }
+    }
+
+}
+
+function removeElement(num){
+    for (i=num; i<elemArr.length-1; i++){
+        $(elemArr[i]).text($(elemArr[i+1]).text());
+    }
+    $(elemArr[c-1]).append("").hide();
+    $(btnArr[c-1]).hide();
+    c--;
+}
+
+function handleEnterKey(e){
+    if(e.keyCode == 13){
+        addStopOver()
     }
 }
 
-function removeItem(target){
-    var element = $("#target");
-    alert(element.val());
-    element.val("");
-    c--;
+function clearAllFields() {
+    var arr = document.getElementsByName("input");
+    for(i=0 ; i<arr.length ; i++){
+        arr[i].value = "";
+    }
+    while(c>0){
+        removeElement(c);
+    }
+    refreshDateAndTime();
+}
+
+window.addEventListener('load', function () {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    var hour = today.getHours();
+    var minutes = today.getMinutes();
+    yyyy = parseInt(yyyy);
+    if(mm < 10){
+        mm = '0' + mm;
+    }
+    if(dd < 10){
+        dd = '0' + dd;
+    }
+    today = yyyy+'-'+mm+'-'+dd;
+    document.getElementById("dateInput").defaultValue = today;
+
+    if(hour < 10){
+        hour = '0' + hour;
+    }
+    if(minutes < 10){
+        minutes = '0' + minutes;
+    }
+
+    today = hour+':'+minutes;
+    document.getElementById("fromTimeInput").defaultValue = today;
+    document.getElementById("toTimeInput").defaultValue = today;
+});
+
+function refreshDateAndTime() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    var hour = today.getHours();
+    var minutes = today.getMinutes();
+    yyyy = parseInt(yyyy);
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementById("dateInput").defaultValue = today;
+
+    if (hour < 10) {
+        hour = '0' + hour;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    today = hour + ':' + minutes;
+    document.getElementById("fromTimeInput").defaultValue = today;
+    document.getElementById("toTimeInput").defaultValue = today;
 }
