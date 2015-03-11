a2f4efb437e40090a21abf354493cd0a2bcb3521
Alex21zel <Alex21zel@gmail.com>
2015-03-11 20:53:48 +0200

    register post to server

---
+++ Scripts/register.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Alex on 3/11/2015.
+ */
+
+function userRegister(){
+    alert("here");
+    var pass = document.getElementById("regPass").value;
+    if(pass == (document.getElementById("regVerPass").value))
+    {
+        var name = document.getElementById("regFName").value;
+        name += " " + document.getElementById("regLName").value;
+        var email = document.getElementById("regEmail").value;
+        var phone = document.getElementById("regPhone").value;
+        sendToServer(name, email, phone, pass)
+    }
+    else{
+        alert("password and verify password not match");
+    }
+}
+
+function sendToServer(name, email, phone, pass){
+    $.ajax({
+        type: "POST",
+        url: "http://rideshare-server.herokuapp.com/user",
+        data: {
+            fullName: name,
+            email: email,
+            phone: phone,
+            password: pass
+        },
+        //contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){alert("SUCC ______" + JSON.stringify(data));},
+        error: function(errMsg) {
+            alert("ERROR ______" + JSON.stringify(errMsg));
+        }
+    });
+}
